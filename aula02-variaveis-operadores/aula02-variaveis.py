097873ba745099f17f1b398537a52a8450379689
isaiasg09 <isaiashsobral@gmail.com>
2026-03-20 22:31:58 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,33 @@
+print("oi")
+
+print(7+4)
+print("7+4")
+print("7"+"4")
+
+# comentario de 1 linha
+'''
+comentario
+de
+varias linhas
+'''
+
+# variaveis
+nome = "isaias" # string
+idade = 18 # int
+peso = 80.1 # float
+print(nome, idade, peso)
+print(f"oi, {nome}, voce tem {idade} anos e pesa {peso}kg")
+
+# inputs
+nome = input("Digite seu nome: ")
+idade = int(input("Digite seu idade: "))
+print(f"oi {nome}, voce tem {idade} anos")
+
+novaidade = idade+1
+print(novaidade)
+
+# Desafio 3:
+n1 = int(input("Digite o numero 1: "))
+n2 = int(input("Digite o numero 2: "))
+print(f"A soma é: {(n1+n2)}")
+
